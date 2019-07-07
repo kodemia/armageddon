fa6e3dbcb8e19254595110edc8791b7f4b2abc20
Carlos Silva <carlos@leco.mx>
2019-07-06 23:38:14 -0500

    koders page

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -14,7 +14,7 @@ class MyDocument extends Document {
           <meta name='viewport' content='width=device-width, initial-scale=1.0' />
           <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
           <meta name='theme-color' content='#1091f3' />
-          <link rel='icon' type='image/png' href='https://cdn.leco.mx/img/identity/favicon-black.png' />
+          <link rel='icon' type='image/png' href='https://kodemia.mx/static/img/logos/isotipo-blanco.png' />
           <link rel='stylesheet' href='https://cdn.materialdesignicons.com/3.7.95/css/materialdesignicons.min.css' />
         </Head>
         <body>
